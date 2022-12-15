631345895b556f08a5ee9ab1beb194f883a6ee58
ALBINATOR-3000 <alyamkhit@gmail.com>
2022-12-15 18:26:10 +0300

    создала сайт с котами, добавила туда своих котов и изменила иконки

---
+++ script/cats.js
@@ -0,0 +1,92 @@
+const cats = [
+    {
+        "id": 76644347893,
+        "name": "Картошка",
+        "age": 4,
+        "description": "Мягкая, трехцветная кошка. Любит облизывать лицо.",
+        "img_link": "/assets/images/popato.jpeg",
+        "rate": 10,
+        "wanted": true
+    },
+    {
+        "id": 96524644893,
+        "name": "Кисель",
+        "age": 1,
+        "description": "Очень маленький и любит воровать с тумбочки резинки для волос.",
+        "img_link": "/assets/images/kisel.jpg",
+        "rate": 6,
+        "wanted": false
+    },
+    {
+        "id": 965898845763,
+        "name": "Звук",
+        "age": 7,
+        "description": "Устраивает самый громкий тыгдык в мире",
+        "img_link": "/assets/images/zvuk.jpeg",
+        "rate": 4,
+        "wanted": false
+    },
+    {
+        "id": 5743892348,
+        "name": "Михаил",
+        "age": 1,
+        "description": "Хочет стать таксистом",
+        "img_link": "/assets/images/mixa.jpeg",
+        "rate": 8,
+        "wanted": true
+    },
+    {
+        "id": 7428722883,
+        "name": "Маруся",
+        "age": 7,
+        "description": "Прекрасная трехцветка, любит курочку и одиночество",
+        "img_link": "/assets/images/maru.jpeg",
+        "rate": 10,
+        "wanted": true
+    },
+    {
+        "id": 9395039275,
+        "name": "Кочан",
+        "age": 4,
+        "description": "Похож на капусту, большой и всегда грязный",
+        "img_link": "/assets/images/kochan.jpeg",
+        "rate": 9,
+        "wanted": false
+    },
+    {
+        "id": 6873923958,
+        "name": "Кучка",
+        "age": 9,
+        "description": "В детстве оставляла везде свои кучки",
+        "img_link": "/assets/images/kucha.jpeg",
+        "rate": 10,
+        "wanted": true
+    },
+    {
+        "id": 57672829499,
+        "name": "Флекс",
+        "age": 3,
+        "description": "Только он знает в чем секрет его друга кота Бориса",
+        "img_link": "/assets/images/flex.jpeg",
+        "rate": 10,
+        "wanted": true
+    },
+    {
+        "id": 3344533324009,
+        "name": "Почка",
+        "age": 3,
+        "description": "Стабильно риносит хозяину 2 мыши в неделю",
+        "img_link": "/assets/images/pochka.jpeg",
+        "rate": 6,
+        "wanted": false
+    },
+    {
+        "id": 328872347834587,
+        "name": "Беляш",
+        "age": 7,
+        "description": "Кот цвета фарша, всегда ворчит и хочет кушать",
+        "img_link": "/assets/images/belyash.jpeg",
+        "rate": 7,
+        "wanted": false
+    }
+]
